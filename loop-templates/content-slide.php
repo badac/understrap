83f198810917b856c327fe492b7deb58f648ff72
decolector <cmart@decolector.net>
2020-11-24 18:10:45 -0500

    cambio en tamaño de imágenes en sliders del home

--- loop-templates/content-slide.php
+++ loop-templates/content-slide.php
@@ -12,7 +12,7 @@
 
 
 <a href="<?php the_permalink(); ?>">
-  <?php echo get_the_post_thumbnail( $post->ID, 'thumbnail' ); ?>
+  <?php echo get_the_post_thumbnail( $post->ID, 'widescreen-md' ); ?>
 </a>
 <header class="entry-header">
 
