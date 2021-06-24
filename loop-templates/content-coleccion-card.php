24ae19ee58353745118f605eb8659afbdd7299f0
decolector <cmart@decolector.net>
2021-06-24 14:53:09 -0500

    card coleccion: título debajo de imagen

--- loop-templates/content-coleccion-card.php
+++ loop-templates/content-coleccion-card.php
@@ -8,7 +8,7 @@
 ?>
 <div class="card">
 	<?php echo the_post_thumbnail('thumbnail', array('class'=>'card-img') ); ?>
-	<div class="card-img-overlay">
+	<div class="card-body">
 		<?php the_title( sprintf( '<h5 class="card-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
 		'</a></h5>' ); ?>
 	</div>
