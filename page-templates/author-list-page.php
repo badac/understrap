db005aef3d44b128f01f73339c2f22b98ce72fb8
decolector <cmart@decolector.net>
2021-07-15 15:50:44 -0500

    contenido de post en plantilla de listado de autor

--- page-templates/author-list-page.php
+++ page-templates/author-list-page.php
@@ -34,7 +34,21 @@ $perfiles = [
 			<div class="col-md-8 content-area" id="primary">
 
 				<main class="site-main" id="main" role="main">
+					<?php while ( have_posts() ) : the_post(); ?>
 
+						<?php get_template_part( 'loop-templates/content', 'page' ); ?>
+
+						<?php
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+
+							comments_template();
+
+						endif;
+						?>
+
+					<?php endwhile; // end of the loop. ?>
+					
 					<?php if (count($perfiles) > 0 ); ?>
 					<div class="row">
 						<div class="card-deck">
